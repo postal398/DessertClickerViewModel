a6a23df134f8e69a5e06993259363e3803a9fc95
Vadim <maslovvadim398@gmail.com>
2023-07-07 10:56:42 +0300

    unnesessary

--- app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.dessertclicker
 
 import android.content.ActivityNotFoundException
@@ -61,6 +45,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.dessertclicker.data.Datasource
 import com.example.dessertclicker.data.DessertUiState
@@ -68,7 +53,6 @@ import com.example.dessertclicker.model.Dessert
 import com.example.dessertclicker.ui.DessertViewModel
 import com.example.dessertclicker.ui.theme.DessertClickerTheme
 
-// Tag for logging
 private const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
@@ -90,8 +74,8 @@ class MainActivity : ComponentActivity() {
 
 
     @Composable
-    private fun DessertClickerApp(
-        viewModel: DessertViewModel = viewModel() //принимаем как параметр viewModel
+    private fun DessertClickerApp(//параметр типа кастомного класса подключенный к
+        viewModel: DessertViewModel = viewModel()  //параметр принимает сам себя? но нет () же
     ) {
         val uiState by viewModel.dessertUiState.collectAsState()
         DessertClickerApp(
